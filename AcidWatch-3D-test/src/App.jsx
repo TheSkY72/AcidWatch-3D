71d518c973986d08859ceb6df592fa4bd45d938a
akshar727 <akshar727@gmail.com>
2026-03-15 12:57:44 -0400

    edit some small things

--- AcidWatch-3D-test/src/App.jsx
+++ AcidWatch-3D-test/src/App.jsx
@@ -1,45 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 import GlobeScene from "./globe/GlobeScene";
 import YearScrubber from "./ui/YearScrubber";
 import ScenarioButtons from "./ui/ScenarioButtons";
 import CO2Slider from "./ui/CO2Slider";
 import DepthPanel from "./panels/DepthPanel";
+import ShellDissolve from "./animations/ShellDissolve";
 
 import { useStore } from "./store";
 
 function App() {
-
   const selectedLocation = useStore((s) => s.selectedLocation);
+  const [testPH, setTestPH] = useState(7.75);
 
   return (
     <div className="app">
-
       <header className="header">
         <h1>AcidWatch</h1>
         <p>Interactive Ocean Acidification Explorer</p>
       </header>
 
       <div className="dashboard">
-
         <div className="globe-card">
           <GlobeScene />
         </div>
 
         <div className="controls-card">
-
           <YearScrubber />
 
           <ScenarioButtons />
 
           <CO2Slider />
-
         </div>
-
       </div>
 
       {selectedLocation && <DepthPanel />}
-
     </div>
   );
 }
